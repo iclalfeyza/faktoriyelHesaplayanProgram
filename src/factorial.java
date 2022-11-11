d2f92b7ff7e7b465fdc5e4f3ed169e662697890b
iclal feyza <iclalfeyzaozeken@hotmail.com>
2022-11-12 00:32:51 +0300

    last

--- src/factorial.java
+++ src/factorial.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class factorial {
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n, total = 1;
+        System.out.print("Please enter a number to calculate its factorial: ");
+        n = scan.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            total *= i;
+        }
+        System.out.println(n + " factorial: " + total);
+    }
 }
